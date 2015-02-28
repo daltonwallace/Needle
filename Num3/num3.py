e3160f56d431694c4fb1a71bf540310773a39618
Dalton Wallace <mivec9@gmail.com>
2015-02-28 10:46:17 -0700

    Nearing completion on number 3

--- Num3/num3.py
+++ Num3/num3.py
@@ -1,42 +1,25 @@
 #!/usr/bin/env
 
 import sys
-import zipfile
-from os.path import basename
 
-# File ordering is not correct
 def main():
 	if len(sys.argv) !=  2:
 		sys.exit("Invalid number of arguments")
+	
+	if not isNumber(sys.argv[1]):
+		sys.exit("Input is not a valid number")
+	
+	num = int(sys.argv[1])
 
-	zipfile = getZipFile(sys.argv[1])
+	
+	print('Original Input: {0}\nHexadecimal: {0:#x}\nBinary: {0:#010b}'.format(num))
 
-	for file in zipfile.infolist():
-		filename = basename(file.filename)
-		if filename != "":
-			print filename, " ",  formatBytes(file.file_size) 
-
-	zipfile.close()
-
-def getZipFile(fileName):
+def isNumber(num):
 	try:
-		result = zipfile.ZipFile(fileName, "r")
-		return result
-	except IOError:
-		sys.exit("Zip File could not be found")
-	except zipfile.BadZipfile:
-		sys.exit("Not A Zip File")
-
-
-def formatBytes(numBytes):
-	if numBytes > 100000:
-		return '{0:.2g} MB'.format(numBytes/ 100000.00)
-
-	if numBytes > 1000:
-		return '{0:.2g} KB'.format(numBytes/ 10000.00)
-
-	return '{0:.2g} bytes'.format(numBytes)
-
+		float(num)
+		return True
+	except ValueError:
+		return False
 
 if __name__ == '__main__':
 	main()
